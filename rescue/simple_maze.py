c6ded7f8ba46b464df15db9cb507343052a172fb
he-lium <dominiche2010@gmail.com>
2016-05-04 18:49:12 +1000

    cleaned up code, added left pathfinding

--- rescue/simple_maze.py
+++ rescue/simple_maze.py
@@ -8,60 +8,73 @@ Akshat Agarwal, Nikodemus Limanuel, Thomas George
 '''
 
 # Import System libraries
-import sys, os, time, threading
+import sys
+import os
+import time
+import threading
 
 # Look for additional libraries in parent dir
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
 # Import ev3dev library
 from ev3dev.auto import *
 
 # Ultrasonic detection threshold
 US_THRESHOLD = 600
+FORWARD = 0
+TURN_RIGHT = 1
+TURN_LEFT = 2
 
 # Connect motors
-right_motor = LargeMotor(OUTPUT_B); assert right_motor.connected
-left_motor = LargeMotor(OUTPUT_C); assert left_motor.connected
-#us_motor = MediumMotor(OUTPUT_B); assert us_motor.connected
-
+right_motor = LargeMotor(OUTPUT_B)
+left_motor = LargeMotor(OUTPUT_C)
+assert right_motor.connected
+assert left_motor.connected
 # Connect sensors
-us = UltrasonicSensor(); assert us.connected
-gyro = GyroSensor(); assert gyro.connected
-color = ColorSensor(); assert color.connected
-push = TouchSensor(); assert push.connected
+us = UltrasonicSensor()
+gyro = GyroSensor()
+color = ColorSensor()
+push = TouchSensor()
 btn = Button()
+assert us.connected
+assert gyro.connected
+assert color.connected
+assert push.connected
+
 
 def run_motors(left, right):
-    left_motor.run_direct(duty_cycle_sp=left*-1)
-    right_motor.run_direct(duty_cycle_sp=right*-1)
+    left_motor.run_direct(duty_cycle_sp=left * -1)
+    right_motor.run_direct(duty_cycle_sp=right * -1)
+
+
+def reverse():
+    run_motors(-35, -35)
+    time.sleep(0.5)
+    stop()
+    time.sleep(0.4)
+
 
 def stop():
     left_motor.stop(stop_command='brake')
     right_motor.stop(stop_command='brake')
 
-path_on_left = lambda : us.value() > US_THRESHOLD
-obstacle_ahead = lambda : push.value() == 1
-red_head = lambda : color.value() == 5
 
-# color.mode = color.MODE_COL_COLOR
-# run_motors(40, 40)
-# while not btn.any():
-#     if obstacle_ahead():
-#         stop()
-#         time.sleep(0.3)
-#         print color.value()
-#         if red_head():
-#             Sound.beep()
-#             time.sleep(0.3)
-#         break
-#     time.sleep(0.1)
-# stop()
+def path_on_left():
+    return us.value() > US_THRESHOLD
+
+
+def obstacle_ahead():
+    return push.value() == 1
+
+
+def red_obstacle():
+    return color.value() == 5
+
 
 class RunMotors(threading.Thread):
-    def __init__(self, rotation):
+    def __init__(self, task):
         threading.Thread.__init__(self)
-        self.rotation = rotation
-        #self.bearing = gyro.value()
+        self.task = task
+        # self.bearing = gyro.value()
         self.adjust = 0
         self.interrupt = False
         self.new_path = False
@@ -69,7 +82,7 @@ class RunMotors(threading.Thread):
     def run(self):
         gyro.mode = 'GYRO-RATE'
         gyro.mode = 'GYRO-ANG'  # Reset gyro
-        if self.rotation == 0: # running straight
+        if self.task == FORWARD:  # running straight
             run_motors(50, 50)
             while not self.interrupt:
                 if not (self.new_path or path_on_left()):
@@ -77,41 +90,49 @@ class RunMotors(threading.Thread):
                 time.sleep(0.1)
                 # add adjustment code
         else:
-            if self.rotation == 1: # rotating right
+            if self.task == TURN_RIGHT:
                 run_motors(40, -40)
-            elif self.rotation == -1: # rotating left
+            elif self.task == TURN_LEFT:
                 run_motors(-40, 40)
             while abs(gyro.value()) < 85 and not self.interrupt:
-                    #print(gyro.value())
-                    time.sleep(0.06)
+                time.sleep(0.06)
             stop()
-            time.sleep(1)
-            print gyro.value()
+
         stop()
         time.sleep(0.5)
+        print gyro.value()
 
     def stop(self):
         self.interrupt = True
 
-try:
-    while True:
-        fred = RunMotors(0)
-        fred.start()
-        while fred.isAlive():
-            if obstacle_ahead():
-                fred.stop()
-                fred.join() # Stop thread, wait to join
-                run_motors(-35, -35)
-                time.sleep(0.5)
-                stop()
-                time.sleep(0.4)
-                fred = RunMotors(1)
-                fred.start()
-                fred.join()
 
+def main():
+    fred = RunMotors(FORWARD)
+    try:
+        while True:
+            fred.start()
+            while fred.isAlive():
+                time.sleep(0.1)
+                if obstacle_ahead():
+                    fred.stop()
+                    fred.join()  # Wait for thread to stop
+                    reverse()
+                    fred = RunMotors(TURN_RIGHT) # Rotate clockwise
+                    fred.start()
+                    fred.join() # wait for turning to complete
+                elif fred.new_path and path_on_left():
+                    time.sleep(0.3) # Wait robot to be fully visible in path
+                    fred.stop()
+                    fred.join()
+                    fred = RunMotors(TURN_LEFT)
+                    fred.start()
+                    fred.join()
+            fred = RunMotors(FORWARD)
 
-except KeyboardInterrupt:
-    fred.stop()
-    fred.join()
+    except KeyboardInterrupt:
+        if fred.isAlive():
+            fred.stop()
+            fred.join()
 
-print gyro.value()
+if __name__ == '__main__':
+    main()
