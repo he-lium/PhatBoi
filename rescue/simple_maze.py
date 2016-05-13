0ca4181e4ef9eac7214e2d395966efb5aa243d00
he-lium <dominiche2010@gmail.com>
2016-05-13 13:07:21 +1000

    adjustment code

--- rescue/simple_maze.py
+++ rescue/simple_maze.py
@@ -19,10 +19,14 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from ev3dev.auto import *
 
 # Ultrasonic detection threshold
-US_THRESHOLD = 400
+US_THRESHOLD = 450
 FORWARD = 0
 TURN_RIGHT = 1
 TURN_LEFT = 2
+
+# Wall adjustment modes
+VEERING_RIGHT = 1
+VEERING_LEFT = -1
 
 # Connect motors
 right_motor = LargeMotor(OUTPUT_B)
@@ -76,25 +80,39 @@ class RunMotors(threading.Thread):
     def __init__(self, task, offset=0):
         threading.Thread.__init__(self)
         self.task = task
-        self.adjust = 0
         self.interrupt = False
         self.new_path = False
         self.running_straight = True
         self.offset = offset
+        self.wall_move = None
+
 
     def run(self):
+        measure_prev = -1
+        measure_time = -1.0
         global rotate_offset
         gyro.mode = 'GYRO-RATE'
         gyro.mode = 'GYRO-ANG'  # Reset gyro
         if self.task == FORWARD:  # running straight
             run_motors(50, 50)
             while not self.interrupt:
-                if not (self.new_path or path_on_left()):
+                if (not self.new_path) and (not path_on_left()):
+                    print "new wall on left", us.value()
                     time.sleep(0.3)
                     self.new_path = True
-                    print "new wall on left", us.value()
+                    measure_prev = us.value();
+                    measure_time = time.time();
                 time.sleep(0.1)
-                # add adjustment code
+                if self.new_path and not (path_on_left()):
+                    if time.time() - measure_time > 0.7:
+                        if us.value()  < 70: # Too close to left wall
+                            self.offset += 6 # Veer right
+                            self.wall_move = VEERING_RIGHT
+                        elif us.value(0) > 250: # Too close to right wall
+                            self.offset -= 6 # Veer left
+                            self.wall_move = VEERING_RIGHT
+                        measure_time = us.value()
+                # adjustment code for right angle
                 if self.running_straight:
                     if gyro.value() + self.offset >= 3:
                         self.running_straight = False
@@ -132,9 +150,6 @@ class RunMotors(threading.Thread):
 
 
 def main():
-    #Sound.beep()
-    #while not btn.any():
-    #    time.sleep(0.3)
     fred = RunMotors(FORWARD)
     try:
         while True:
@@ -159,7 +174,6 @@ def main():
                     while fred.isAlive():
                         time.sleep(0.1) # wait for turning to complete
                 elif fred.new_path and path_on_left():
-                    #Sound.beep()
                     print "new path on left: us=", us.value()
                     left_motor.position = 0
                     right_motor.position = 0
@@ -174,7 +188,9 @@ def main():
                     while fred.isAlive():
                         time.sleep(0.1)
             fred = RunMotors(FORWARD, rotate_offset)
-
+        if fred.isAlive():
+            fred.stop()
+            fred.join()
     except KeyboardInterrupt:
         #rotation_val = (left_motor.position + right_motor.position) / 2
         #print "rotation: ", rotation_val
